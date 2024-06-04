d120c0e70074819cfac0f881c8ead931012ded2f
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2024-06-04 08:01:41 -0500

    Ejemplo de expresiones regulares

---
+++ unidad_02/taller_02/src/App.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String cadena = "[a-z]{5,10}[0-9]*@(ups.edu.ec|est.ups.edu.ec)";
+        Pattern patron = Pattern.compile(cadena);
+
+        String ejemplo = "gpizarro1@est.ups.edu.ec";
+        Matcher verificador = patron.matcher(ejemplo);
+
+        while(verificador.find()) {
+            System.out.println( verificador.group() + " " + verificador.start() );
+        }
+
+        System.out.println( ejemplo.matches(cadena) );
+    }
+}
