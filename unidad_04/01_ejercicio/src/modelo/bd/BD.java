0672e2cddb401ad3cc01f6466d58109fa06e4e1e
guillermopizarro <gpizarro@espol.edu.ec>
2024-07-25 08:57:58 -0500

    Conexion a la Base de Datos

---
+++ unidad_04/01_ejercicio/src/modelo/bd/BD.java
@@ -0,0 +1,36 @@
+package modelo.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BD {
+
+    private Conexion conexion;
+    private Connection con;
+    private Archivo archivo;
+
+    public BD() {
+        this.archivo = new Archivo();
+        this.archivo.leerArchivo();
+    }
+
+    public void conectar() {
+        try {
+            Class.forName(this.archivo.getConexion().getDriverClass());
+            this.con = DriverManager.getConnection( this.archivo.getConexion().urlConexion(), 
+                this.archivo.getConexion().getUsuario(), 
+                this.archivo.getConexion().getClave() );
+            System.out.println( "[db] - Conexion exitosa." );
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println( "[error] - " + e.getMessage() );
+        }
+    }
+
+    public static void main(String[] args) {
+        BD bd = new BD();
+        bd.conectar();
+    }
+}
